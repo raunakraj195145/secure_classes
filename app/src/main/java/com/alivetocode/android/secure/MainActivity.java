f52df43170f9e2bd039f48ff1c6e47cdfe25793e
Deepansh <kumardeepansh210@gmail.com>
2021-06-03 15:04:52 +0530

    removed button from mainacitvity

--- app/src/main/java/com/alivetocode/android/secure/MainActivity.java
+++ app/src/main/java/com/alivetocode/android/secure/MainActivity.java
@@ -8,25 +8,12 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Added temporary button
-        button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+    }
 
-            public void onClick(View v) {
-                openLoginOptions();
-            }
-        });
-    }
-    //Function for going to next activity
-    public void openLoginOptions() {
-        Intent intent = new Intent(this, LoginOptions.class);
-        startActivity(intent);
-    }
 }
